95dc741779915623fa73e3fce6754233b3bfdc0a
Oh, Kwangshin | Teddy | ECMD <kwangshin.oh@rakuten.com>
2017-01-31 00:07:40 +0900

    Change the static list into code.

--- utils.py
+++ utils.py
@@ -3,13 +3,17 @@ cols = '123456789'
 
 def cross(a, b):
     return [s+t for s in a for t in b]
+def diagonal_linear(a, b):
+    return [a[i]+b[i] for i in range(9)]
+def diagonal_backward_linear(a, b):
+    return [a[i]+b[8-i] for i in range(9)]
 
 boxes = cross(rows, cols)
 
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-diagonal_units = [['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9'], ['A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1']]
+diagonal_units = [diagonal_linear(rows, cols), diagonal_backward_linear(rows, cols)]
 unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
